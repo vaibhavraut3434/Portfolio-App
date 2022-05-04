6ca4d1962050ec909be7e3048a91d60354d53517
Vaibhav <vaibhavraut3434@gmail.com>
2022-05-04 11:00:21 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Vaibhav. I am a Software Engineer.</p>
+                <p>I'm always looking forward to work on meaningful projects.</p>
+            </div>
+        )
+    }
 }
-
-export default App;
